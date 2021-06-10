e23b4067e04f91f4ed8d2f0c7bbdf63aeb0bc969
Lingjie-Liu <72461234+Lingjie-Liu@users.noreply.github.com>
2021-06-10 18:11:23 -0400

    Update find_common_expressed_genes.R

--- scripts/find_common_expressed_genes.R
+++ scripts/find_common_expressed_genes.R
@@ -1,87 +1,98 @@
-### This script is to filterout low expressed gene          #############
+### This script is to filter out low expressed gene          #############
 ### abundance unit: sum abundance of tx in dominant promoter model ######
-### sumarize the distribution of abudance unit and remove genes  ########
-### with small abudance unit                                #############
+### summarize the distribution of abundance unit and remove genes  ########
+### with small abundance unit                                #############
 
 library(stringr)
 library(GenomicRanges)
+library(data.table)
 
-output_dir = "C:/Users/lenovo/Desktop/unified_model/data/"
+root_dir = "C:/Users/ling/Dropbox/scripts"
+output_dir = file.path(root_dir, "unified_model/data/")
 
-hs_grng = readRDS("C:/Users/lenovo/Desktop/unified_model/data/hsapiens_transcript_grng.RDS")
+hs_grng = readRDS(file.path(output_dir, 'hsapiens_transcript_grng.RDS'))
 
 ##filter out genes with low expression #####
 sel_chr <- sort(str_subset(unique(seqnames(hs_grng)), "^[0-9]+.?$|^X"))
+
 # filter out non-protein-coding genes, and genes shorter than 10kb 
+min_gene_length = 10000
 grng_filtered <-
   hs_grng[(seqnames(hs_grng) %in% sel_chr) &
-            (hs_grng$gene_biotype == "protein_coding") & width(hs_grng) >= 10000]
+            (hs_grng$gene_biotype == "protein_coding") & width(hs_grng) >= min_gene_length]
 
 # get protein coding genes from filtered grng
 pc_gene = unique(grng_filtered$ensembl_gene_id)
 
 
-###### for cd14 : get the sum_abudance of dominant promoter model ##############
+###### for cd14 : get the sum_abundance of dominant promoter model #############
 ###### filter out those with low expression ####################################
-cd14_ta <- read.csv("C:/Users/lenovo/Desktop/unified_model/data/PROseq-HUMAN-CD14-3-1_grouping.txt",
-                    sep = '\t', header = T, stringsAsFactors = F)
-colnames(cd14_ta) = c('tx_id', 'tss', 'tts', 'ab', 'model', 'gene_id')
+read_ab_file <- function(file_name){
+  file = read.csv(file.path(output_dir, file_name),
+                  sep = '\t', header = T, stringsAsFactors = F,
+                  col.names = c('tx_id', 'tss', 'tts', 'ab', 'model', 'gene_id'))
+  return(file)
+}
+cd14_ta = read_ab_file('PROseq-HUMAN-CD14-3-1_grouping.txt')
+cd4_ta = read_ab_file('PROseq-HUMAN-CD4-3-1_grouping.txt')
+
 #filter with protein coding genes
 cd14_ta <- cd14_ta[cd14_ta$gene_id %in% pc_gene, ]
-
-########### for cd14 #######################
-#cd14_expression = c()
-cd14_expressed_genes = c()
-for (gene in unique(cd14_ta$gene_id)) {
-  sub_df = cd14_ta[cd14_ta$gene_id == gene, ]
-  tss_set = sub_df$tss
-  model_sum = c()
-  
-  for (i in unique(tss_set)){
-    model_sum = c(model_sum, i, sum(sub_df[sub_df$tss ==i, ]$ab))
-    
-  }
-  
-  dominant_ab = max(model_sum[c(F,T)])
-  
-  #cd14_expression = c(cd14_expression, dominant_ab) #summarize expression level 
-  
-  if(dominant_ab > 10){
-    cd14_expressed_genes = c(cd14_expressed_genes, gene)
-  }
-  
-}
-#summary(cd14_expression) # Median is 5.113, expression threshold is 10 
-
-####### for cd4 #################
-cd4_ta <- read.csv("C:/Users/lenovo/Desktop/unified_model/data/PROseq-HUMAN-CD4-3-1_grouping.txt",
-                    sep = '\t', header = T, stringsAsFactors = F)
-colnames(cd4_ta) = c('tx_id', 'tss', 'tts', 'ab', 'model', 'gene_id')
-#filter with protein coding genes
 cd4_ta <- cd4_ta[cd4_ta$gene_id %in% pc_gene, ]
 
-#cd4_expression = c()
-cd4_expressed_genes = c()
-for (gene in unique(cd4_ta$gene_id)) {
-  sub_df = cd4_ta[cd4_ta$gene_id == gene, ]
-  tss_set = sub_df$tss
-  model_sum = c()
-  
-  for (i in unique(tss_set)){
-    model_sum = c(model_sum, i, sum(sub_df[sub_df$tss ==i, ]$ab))
+########### summarize the expression of dominant tx in each gene ###############
+summarize_expression <- function(tx_ab){
+  expression = c()
+  for (gene in unique(tx_ab$gene_id)) {
+    sub_df = tx_ab[tx_ab$gene_id == gene, ]
+    tss_set = sub_df$tss
+    model_sum = c()
     
-  }
+    for (i in unique(tss_set)){
+      model_sum = c(model_sum, i, sum(sub_df[sub_df$tss ==i, ]$ab))
+      
+    }
+    
+    dominant_ab = max(model_sum[c(F,T)]) # summarize the expression of dominant tx
+    expression = c(expression, dominant_ab)  
   
-  dominant_ab = max(model_sum[c(F,T)])
+    }
   
-  #cd4_expression = c(cd4_expression, dominant_ab) #summarize expression level
-  
-  if(dominant_ab > 10){
-    cd4_expressed_genes = c(cd4_expressed_genes, gene)
-  }
-  
+  return(expression)
 }
-#summary(cd4_expression) # Median is 8.002, so expression threshold is 10
 
+# call summarize_expression for each sample and check the distribution 
+cd14_expression = summarize_expression(cd14_ta)
+summary(cd14_expression)
+cd4_expression = summarize_expression(cd4_ta)
+summary(cd4_expression)
+
+############# pick genes with relatively high expression 
+pick_expressed_genes <- function(tx_ab, threshold){
+  expressed_genes = c()
+  for (gene in unique(tx_ab$gene_id)) {
+    sub_df = tx_ab[tx_ab$gene_id == gene, ]
+    tss_set = sub_df$tss
+    model_sum = c()
+    
+    for (i in unique(tss_set)){
+      model_sum = c(model_sum, i, sum(sub_df[sub_df$tss ==i, ]$ab))
+      
+    }
+    
+    dominant_ab = max(model_sum[c(F,T)]) # summarize the expression of dominant tx
+     
+    if(dominant_ab > threshold){
+      expressed_genes = c(expressed_genes, gene)
+    }
+  }
+  
+  return(expressed_genes)
+}
+
+threshold = 10 # set minimal expression level
+cd14_expressed_genes = pick_expressed_genes(cd14_ta, threshold)
+cd4_expressed_genes = pick_expressed_genes(cd4_ta, threshold)
 common_expressed_genes = intersect(cd14_expressed_genes, cd4_expressed_genes) #6916
+
 saveRDS(common_expressed_genes, paste0(output_dir, "common_expressed_genes.RData"))
