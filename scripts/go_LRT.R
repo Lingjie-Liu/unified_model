6fb90790c0ea450148700031c592027283b5212b
Lingjie-Liu <72461234+Lingjie-Liu@users.noreply.github.com>
2021-06-14 14:45:17 -0400

    Update go_LRT.R

--- scripts/go_LRT.R
+++ scripts/go_LRT.R
@@ -59,6 +59,12 @@ p_values = result[c(F, T)]
 q_values =  p.adjust(p_values, method = "BH")
 OR = (test_genes$cd14_tss/test_genes$cd14_gb)/(test_genes$cd4_tss/test_genes$cd4_gb)
 
+lrt_beta_cols = c('gene_id', 'cd14_tss', 'cd14_gb','cd14_tts',
+                  'cd4_tss', 'cd4_gb', 'cd4_tts', '2T', 
+                  'p_value', 'q_value', 'odds_ratio',
+                  'cd14_alpha', 'cd14_beta', 'cd14_gamma', 'cd14_lambda',
+                  'cd4_alpha', 'cd4_beta', 'cd4_gamma', 'cd4_lambda')
+
 testing_results = data.table(test_genes$gene_id, test_genes$cd14_tss,
                              test_genes$cd14_gb, test_genes$cd14_tts,
                              test_genes$cd4_tss,test_genes$cd4_gb, 
@@ -66,19 +72,17 @@ testing_results = data.table(test_genes$gene_id, test_genes$cd14_tss,
                              cd14_alpha, cd14_beta, cd14_gamma, cd14_lambda,
                              cd4_alpha, cd4_beta, cd4_gamma, cd4_lambda)
 write.table(testing_results, paste0(output_dir, 'human_LRTbeta_results.tsv'), 
-            col.names = c('gene_id', 'cd14_tss', 'cd14_gb','cd14_tts',
-                          'cd4_tss', 'cd4_gb', 'cd4_tts', '2T', 
-                          'p_value', 'q_value', 'odds_ratio',
-                          'cd14_alpha', 'cd14_beta', 'cd14_gamma', 'cd14_lambda',
-                          'cd4_alpha', 'cd4_beta', 'cd4_gamma', 'cd4_lambda'),
+            col.names = lrt_beta_cols,
             row.names = F,sep = '\t', quote = F)
 
 ## choose q cutoff 0.01 ##
 testing_0.01 = testing_results[which(testing_results$q_values < 0.01), ] # number 1417
+colnames(testing_0.01) = lrt_beta_cols
 saveRDS(testing_0.01, paste0(output_dir, 'human_LRTbeta_0.01.RDS'))
 
 ## choose q cutoff 0.01, OR > 2 or OR < 0.5 ##
-testing_0.01_OR = testing_0.01[which(testing_0.01$OR < 0.5 | testing_0.01$OR > 2), ] # number 1210
+testing_0.01_OR = testing_0.01[which(testing_0.01$odds_ratio < 0.5
+                                     | testing_0.01$odds_ratio > 2), ] # number 1210
 saveRDS(testing_0.01_OR, paste0(output_dir, 'human_LRTbeta_0.01_OR.RDS'))
 
 
@@ -108,6 +112,12 @@ p_values = result[c(F, T)]
 q_values =  p.adjust(p_values, method = "BH")
 
 # there is no OR for alpha testing
+lrt_alpha_cols = c('gene_id', 'cd14_tss', 'cd14_gb','cd14_tts',
+                  'cd4_tss', 'cd4_gb', 'cd4_tts', 
+                  '2T', 'p_value', 'q_value', 
+                  'cd14_alpha', 'cd14_beta', 'cd14_gamma', 'cd14_lambda',
+                  'cd4_alpha', 'cd4_beta', 'cd4_gamma', 'cd4_lambda')
+
 testing_results = data.table(test_genes$gene_id, test_genes$cd14_tss,
                              test_genes$cd14_gb, test_genes$cd14_tts,
                              test_genes$cd4_tss,test_genes$cd4_gb, 
@@ -115,13 +125,22 @@ testing_results = data.table(test_genes$gene_id, test_genes$cd14_tss,
                              cd14_alpha, cd14_beta, cd14_gamma, cd14_lambda,
                              cd4_alpha, cd4_beta, cd4_gamma, cd4_lambda)
 write.table(testing_results, paste0(output_dir, 'human_LRTalpha_results.tsv'), 
-            col.names = c('gene_id', 'cd14_tss', 'cd14_gb','cd14_tts',
-                          'cd4_tss', 'cd4_gb', 'cd4_tts', 
-                          '2T', 'p_value', 'q_value', 
-                          'cd14_alpha', 'cd14_beta', 'cd14_gamma', 'cd14_lambda',
-                          'cd4_alpha', 'cd4_beta', 'cd4_gamma', 'cd4_lambda'),
+            col.names = lrt_alpha_cols,
             row.names = F,sep = '\t', quote = F)
 
 ## choose q cutoff 0.01 ##
 testing_0.01 = testing_results[which(testing_results$q_values < 0.01), ] # number 3573
+colnames(testing_0.01) = lrt_alpha_cols
 saveRDS(testing_0.01, paste0(output_dir, 'human_LRTalpha_0.01.RDS'))
+
+# determine differential pausing only cases and differential initiation cases
+beta_cases = readRDS(paste0(output_dir, 'human_LRTbeta_0.01_OR.RDS'))
+alpha_cases = readRDS(paste0(output_dir, 'human_LRTalpha_0.01.RDS'))
+
+pausing_initiation = intersect(beta_cases$gene_id, alpha_cases$gene_id)
+pausing_only = beta_cases[! beta_cases$gene_id %in% pausing_initiation, ]
+initiation_only = alpha_cases[! alpha_cases$gene_id %in% pausing_initiation, ]
+
+saveRDS(pausing_initiation, paste0(output_dir, 'pausing_initiation_union.RData'))
+saveRDS(pausing_only, paste0(output_dir, 'pausing_only.RData'))
+saveRDS(initiation_only, paste0(output_dir, 'initiation_only.RData'))
