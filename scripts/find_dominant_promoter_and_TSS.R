370ffce0de347ff85d5860b3a58a019b9260380e
Yixin <yizhao@cshl.edu>
2021-06-22 19:14:51 -0400

    WIP find dominant promoter and TSS

---
+++ scripts/find_dominant_promoter_and_TSS.R
@@ -0,0 +1,15 @@
+#### log file ####
+log <- file(snakemake@log[[1]], open="wt")
+sink(file = log, type = "output")
+sink(file = log, type = "message")
+
+#### load packages ####
+
+#### snakemake files ####
+
+#### testing files ####
+sample_1
+
+
+#### end of parsing arguments ####
+
