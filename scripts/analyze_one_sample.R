692005ce6f7ea2a4e9a727a84e4a2cd75fd7cb5d
Yixin <yizhao@cshl.edu>
2021-07-27 18:31:42 -0400

    WIP analyze single sample

---
+++ scripts/analyze_one_sample.R
@@ -0,0 +1,306 @@
+#### log file ####
+log <- file(snakemake@log[[1]], open="wt")
+sink(file = log, type = "output")
+sink(file = log, type = "message")
+
+#### load packages ####
+library(tidyverse)
+library(GenomicRanges)
+library(rtracklayer)
+library(ggpubr)
+
+#### snakemake files ####
+tq_in <- snakemake@input[["tq"]]
+tid1_in <- snakemake@input[["tid1"]]
+tid2_in <- snakemake@input[["tid2"]]
+
+bwp1_p5_in <- snakemake@input[["bwp1_p5"]]
+bwm1_p5_in <- snakemake@input[["bwm1_p5"]]
+bwp1_p3_in <- snakemake@input[["bwp1_p3"]]
+bwm1_p3_in <- snakemake@input[["bwm1_p3"]]
+
+bwp2_p5_in <- snakemake@input[["bwp2_p5"]]
+bwm2_p5_in <- snakemake@input[["bwm2_p5"]]
+bwp2_p3_in <- snakemake@input[["bwp2_p3"]]
+bwm2_p3_in <- snakemake@input[["bwm2_p3"]]
+
+tid_cutoff <- snakemake@params[["tid_cutoff"]] # length cutoff for how long a TID containing multiple TSSs could span
+tsn_cutoff <- snakemake@params[["tsn_cutoff"]]
+pause_cutoff <- snakemake@params[["pause_cutoff"]] 
+gb_min_length <- snakemake@params[["gb_min_length"]] 
+tts_length <- snakemake@params[["tts_length"]] # parameter m
+quantile_normalization <- snakemake@params[["quantile_normalization"]] # "identity", "qnorm"
+
+result_dir <- snakemake@params[["result_dir"]] 
+
+alpha_out <- snakemake@output[["alpha"]] 
+beta_out <- snakemake@output[["beta"]] 
+
+#### testing files ####
+root_dir <- "~/Desktop/github/unified_model"
+
+tq_in <- file.path(root_dir, "data/tq/human_rhesus/template-26.RDS")
+tid1_in <- file.path(root_dir, "results/tidgrng/PROseq-HUMAN-CD14-26.RDS")
+
+bwp1_p5_in <- file.path(root_dir, "data/bigwig/p5/human_rhesus/PROseq-HUMAN-CD14_plus.bw")
+bwm1_p5_in <- file.path(root_dir, "data/bigwig/p5/human_rhesus/PROseq-HUMAN-CD14_minus.bw")
+bwp1_p3_in <- file.path(root_dir, "data/bigwig/p3/human_rhesus/PROseq-HUMAN-CD14_plus.bw")
+bwm1_p3_in <- file.path(root_dir, "data/bigwig/p3/human_rhesus/PROseq-HUMAN-CD14_minus.bw")
+
+tid_cutoff <- 1000 # length cutoff for how long a TID containing multiple TSSs could span
+tsn_cutoff <- 5
+pause_cutoff <- 250
+
+quantile_normalization <- "identity"
+
+# gb_start <- 2000
+# gb_length <- 6000 # parameter l
+
+gb_min_length <- 1e4
+tts_length <- pause_cutoff # parameter m
+
+result_dir <-
+  file.path(root_dir, "results/within_sample", "HUMAN-CD14")
+dir.create(result_dir, showWarnings = FALSE, recursive = TRUE)
+
+alpha_out <- file.path(result_dir, "alpha.csv")
+beta_out <- file.path(result_dir, "beta.csv")
+
+#### end of parsing arguments ####
+dir.create(result_dir, showWarnings = FALSE, recursive = TRUE)
+
+#### functions ####
+# quantile normalization 
+normalize.quantiles <-
+  function(x) {
+    ## validate inputs
+    x <- as.matrix(x)
+    stopifnot(
+      is.numeric(x),
+      !anyNA(x)
+    )
+    
+    ## quantile normalize
+    m <- apply(x, 2, function(v) v[order(v)])
+    dim(m) <- dim(x)    # apply() doesn't always return a matrix!
+    
+    row_mean <- rowMeans(m)
+    
+    result <- apply(
+      x, 2, function(v, row_mean) row_mean[order(order(v))], row_mean
+    )
+    dim(result) <- dim(x)
+    
+    ## propagate dimnames
+    dimnames(result) <- dimnames(x)
+    return(result)
+  }
+
+#### generate regions for read counting ####
+# get union TSS regions for two samples
+tid1 <- readRDS(tid1_in)
+
+# filter out tid with length larger than a certain thredhold
+tid1 <- tid1[width(tid1) < tid_cutoff]
+
+# resize to a fixed region for searching highest PRO-seq counts
+tid_resize <- resize(tid1, width = 3000, fix = "center")
+
+# import and process bigwigs for 5' end
+bwp1_p5 <- import.bw(bwp1_p5_in)
+bwm1_p5 <- import.bw(bwm1_p5_in)
+
+strand(bwp1_p5) <- "+"
+
+# clean up reads from minus strand
+process_bwm <- function(bwm) {
+  strand(bwm) <- "-"
+  bwm$score <- abs(bwm$score)
+  bwm <- bwm[bwm$score > 0]
+  return(bwm)
+}
+
+bwm1_p5 <- process_bwm(bwm1_p5)
+
+bw1_p5 <- c(bwp1_p5, bwm1_p5)
+
+rm(bwp1_p5, bwm1_p5)
+
+# get transcription start nucleotide (TSN)
+get_max_tsn <- function(bw_p5) {
+  ovp <- findOverlaps(tid_resize, bw_p5)
+  bw_p5 <- bw_p5[subjectHits(ovp)]
+  bw_p5$ensembl_gene_id <- tid_resize[queryHits(ovp)]$ensembl_gene_id
+  bw_p5 <- bw_p5 %>%
+    plyranges::group_by(ensembl_gene_id) %>%
+    filter(score == max(score)) %>%
+    ungroup()
+  return(bw_p5)
+}
+
+bw1_tsn <- get_max_tsn(bw1_p5)
+
+message("Number of 5' end reads across all sites:")
+summary(bw1_p5$score)
+message("Number of 5' end reads around TSSs:")
+summary(bw1_tsn$score)
+
+# filter out genes with low counts around TSS
+bw1_tsn <- bw1_tsn[bw1_tsn$score > tsn_cutoff]
+
+# use genes with only one single highest TSN
+get_genes_with_single_tsn <- function(bw_tsn) {
+  bw_tsn_ct <- table(bw_tsn$ensembl_gene_id)
+  names(bw_tsn_ct[bw_tsn_ct == 1])
+}
+
+bw1_single_tsn <- get_genes_with_single_tsn(bw1_tsn)
+
+bw1_tsn <- sort(bw1_tsn[bw1_tsn$ensembl_gene_id %in% bw1_single_tsn])
+
+seqlevels(bw1_tsn) <- seqlevelsInUse(bw1_tsn)
+
+# get TSNs downstream regions for pause peak
+bw1_pause <- promoters(bw1_tsn, upstream = 0, downstream = pause_cutoff)
+bw1_pause$score <- NULL
+
+# # get gene body region by fixed length
+# bw_gb <- bw_pause %>% plyranges::shift_downstream(gb_start)
+# bw_gb <- bw_gb %>% plyranges::anchor_5p() %>% mutate(width = gb_length)
+
+# get TTS by using annotated gene regions
+tq <- readRDS(tq_in)
+txgrng <- tq@transcripts
+
+txgrng <- txgrng[txgrng$ensembl_gene_id %in% bw1_pause$ensembl_gene_id]
+gngrng <- txgrng %>%
+  plyranges::group_by(ensembl_gene_id) %>%
+  plyranges::reduce_ranges_directed() %>%
+  sort()
+
+seqlevels(gngrng) <- seqlevelsInUse(gngrng)
+
+# bw_tts <- gngrng %>% plyranges::anchor_3p() %>% mutate(width = 1)
+bw_tts <- gngrng %>% plyranges::anchor_3p() %>% mutate(width = 250)
+
+# get gene body region by pause and termination sites
+bw_pause_end <- bw1_pause %>% plyranges::anchor_3p() %>% mutate(width = 1)
+bw_tts_end  <- bw_tts %>% plyranges::anchor_5p() %>% mutate(width = 1)
+
+seqlevels(bw_pause_end) <- seqlevels(bw_tts_end)
+bw_pause_end <- sort(bw_pause_end)
+bw_tts_end <- sort(bw_tts_end)
+
+bw_gb <- punion(bw_pause_end, bw_tts_end, fill.gap = TRUE)
+bw_gb$ensembl_gene_id <- bw_pause_end$ensembl_gene_id
+
+bw_gb_filtered <- bw_gb[width(bw_gb) > gb_min_length]
+# trim either end to avoid pausing and termination peaks
+bw_gb_filtered <- bw_gb_filtered - 2000
+
+bw_pause_filtered <-
+  bw1_pause[bw1_pause$ensembl_gene_id %in% bw_gb_filtered$ensembl_gene_id]
+bw_tts_filtered <-
+  bw_tts[bw_tts$ensembl_gene_id %in% bw_gb_filtered$ensembl_gene_id]
+
+# # make sure gene body doesn't exceed TTS
+# bw_gb_end <- bw_gb %>% plyranges::anchor_3p() %>% mutate(width = 1)
+# 
+# bw_gb_end$indicator <- (start(bw_tts) > start(bw_gb_end))
+# 
+# gn_filter <-
+#   ifelse((strand(bw_gb_end) == "+" & bw_gb_end$indicator) |
+#            (strand(bw_gb_end) == "-" & !bw_gb_end$indicator),
+#        TRUE, FALSE)
+# 
+# # get three regions for read counting
+# bw_gb_filtered <- bw_gb[gn_filter]
+# 
+# bw_tts_filtered <-
+#   bw_tts[bw_tts$ensembl_gene_id %in% bw_gb_filtered$ensembl_gene_id] %>%
+#   plyranges::anchor_3p() %>%
+#   mutate(width = tts_length)
+# 
+# bw_pause_filtered <- bw_pause[bw_pause$ensembl_gene_id %in% bw_gb_filtered$ensembl_gene_id]
+
+count_grng <- sort(c(bw_pause_filtered, bw_gb_filtered, bw_tts_filtered))
+saveRDS(count_grng, file = file.path(result_dir, "granges_for_read_counting.RDS"))
+
+# import and process bigwigs for 3' end
+bwp1_p3 <- import.bw(bwp1_p3_in)
+bwm1_p3 <- import.bw(bwm1_p3_in)
+
+strand(bwp1_p3) <- "+"
+
+bwm1_p3 <- process_bwm(bwm1_p3)
+
+bw1_p3 <- c(bwp1_p3, bwm1_p3)
+
+# summarize read counts
+summarise_bw <-
+  function(bw, grng, col_name) {
+    rc <- grng %>%
+      plyranges::group_by_overlaps(bw) %>%
+      plyranges::group_by(ensembl_gene_id) %>% 
+      plyranges::summarise(score = sum(score))
+    colnames(rc) <- c("gene_id", col_name)
+    return(rc)
+  }
+
+rc1_pause <- summarise_bw(bw1_p3, bw_pause_filtered, "sp1")
+rc1_pause$pause_length <-
+  width(bw_pause_filtered)[match(rc1_pause$gene_id, bw_pause_filtered$ensembl_gene_id)]
+
+rc1_gb <- summarise_bw(bw1_p3, bw_gb_filtered, "sb1")
+
+rc1_gb$gb_length <-
+  width(bw_gb_filtered)[match(rc1_gb$gene_id, bw_gb_filtered$ensembl_gene_id)]
+
+rc1_tts <- summarise_bw(bw1_p3, bw_tts_filtered, "st1")
+
+#### Poisson-based Maximum Likelihood Estimation ####
+rc1 <- Reduce(function(x, y) merge(x, y, by = "gene_id", all = TRUE),
+               list(rc1_pause, rc1_gb, rc1_tts))
+
+# Use all analyzed genes
+# lambda_1 <- sum(rc1$sb1, na.rm = TRUE) / (sum(!is.na(rc1$sb1)) * gb_length)
+# lambda_2 <- sum(rc2$sb2, na.rm = TRUE) / (sum(!is.na(rc2$sb2)) * gb_length)
+
+# Use all loci
+if (quantile_normalization == "identity") {
+  lambda_1 <- (sum(bwp1_p3$score) + sum(bwm1_p3$score)) / (sum(width(bwp1_p3)) + sum(width(bwm1_p3)))
+  
+  alpha_1 <- rc1$sb1 / (rc1$gb_length * lambda_1)
+  beta_1 <- (rc1$sb1 / rc1$gb_length) / (rc1$sp1 / rc1$pause_length)
+  gamma_1 <- (rc1$sb1 / rc1$gb_length) / (rc1$st1 / tts_length)
+}
+
+rate_tbl <-
+  tibble(gene_id = rc1$gene_id,
+         alpha = alpha_1,
+         beta = beta_1, 
+         gamma = gamma_1)
+
+write_csv(rate_tbl, file = file.path(result_dir, "rate.csv"))
+
+#### visualize results ####
+# scatter plot with margin
+# https://stackoverflow.com/questions/8545035/scatterplot-with-marginal-histograms-in-ggplot2
+p <- rate_tbl %>%
+  mutate(across(where(is.numeric), ~log2(.))) %>% 
+  ggscatterhist(x = "alpha", y = "beta",
+            # color = "black", shape = 21, size = 3, # Points color, shape and size
+            add = "reg.line",  # Add regressin line
+            add.params = list(color = "blue", fill = "lightgray"), # Customize reg. line
+            conf.int = TRUE, # Add confidence interval
+            cor.coef = TRUE, # Add correlation coefficient. see ?stat_cor
+            cor.coef.coord = c(-12, 0),
+            cor.coeff.args = list(method = "pearson", label.x = 3, label.sep = "\n"),
+            alpha = 0.1,
+            xlab = "log2(alpha)",
+            ylab = "log2(beta)"
+  )
+# convert the plot list into a single plot
+p <- print(p)
+ggsave(filename = file.path(result_dir, "alpha_vs_beta_scatter.pdf"), plot = p,
+       width = 5, height = 5)
--- scripts/do_gse.R
+++ scripts/do_gse.R
@@ -4,10 +4,12 @@ sink(file = log, type = "output")
 sink(file = log, type = "message")
 
 #### load packages ####
+library(clusterProfiler)
+library(enrichplot)
+library(org.Hs.eg.db)
 library(magrittr)
 library(readr)
-library(clusterProfiler)
-library(org.Hs.eg.db)
+library(dplyr)
 
 #### snakemake files ####
 
@@ -15,60 +17,94 @@ library(org.Hs.eg.db)
 #### testing files ####
 root_dir <- "~/Desktop/github/unified_model"
 
-alpha_in <-
-  file.path(root_dir,
-            "results/between_samples/PROseq-HUMAN-CD4_vs_PROseq-HUMAN-CD14/S26-qnorm/alpha.csv")
+# alpha_in <-
+#   file.path(root_dir,
+#             "results/between_samples/PROseq-HUMAN-CD4_vs_PROseq-HUMAN-CD14/S26-qnorm/alpha.csv")
+# 
+# beta_in <-
+#   file.path(root_dir,
+#             "results/between_samples/PROseq-HUMAN-CD4_vs_PROseq-HUMAN-CD14/S26-qnorm/beta.csv")
 
-beta_in <-
-  file.path(root_dir,
-            "results/between_samples/PROseq-HUMAN-CD4_vs_PROseq-HUMAN-CD14/S26-qnorm/beta.csv")
+rate_in <-
+  file.path(root_dir, "results/within_sample/HUMAN-CD14/rate.csv")
 
 #### end of parsing arguments ####
-alpha_tbl <- read_csv(alpha_in, col_types = cols(gene_id = col_character()))
-beta_tbl <- read_csv(beta_in, col_types = cols(gene_id = col_character()))
+# alpha_tbl <- read_csv(alpha_in, col_types = cols(gene_id = col_character()))
+# beta_tbl <- read_csv(beta_in, col_types = cols(gene_id = col_character()))
 
-rate_tbl <- alpha_tbl
+rate_tbl <- read_csv(rate_in, col_types = cols(gene_id = col_character()))
 
-rate_tbl <- rate_tbl %>%
-  mutate(t_sort = ifelse(lfc >= 0, t, -t)) %>%
-  arrange(desc(t_sort))
+# rate_tbl <- rate_tbl %>%
+#   mutate(t_sort = ifelse(lfc >= 0, t, -t)) %>%
+#   arrange(desc(t_sort))
+# 
+# geneList <- rate_tbl$t_sort
+# names(geneList) <- rate_tbl$gene_id
 
-geneList <- rate_tbl$t_sort
+rate_tbl <- rate_tbl %>% arrange(desc(alpha))
+geneList <- rate_tbl$alpha
 names(geneList) <- rate_tbl$gene_id
 
-ego <- gseGO(geneList     = geneList,
+ego <- gseGO(geneList     = sort(geneList, decreasing = TRUE),
               OrgDb        = org.Hs.eg.db,
               keyType = "ENSEMBL",
-              ont          = "CC",
-              # minGSSize    = 100,
-              # maxGSSize    = 500,
+              ont          = "BP",
+              minGSSize    = 100,
+              maxGSSize    = 500,
               pvalueCutoff = 0.05,
-              verbose      = TRUE)
+              verbose      = TRUE,
+              scoreType = "pos")
 
-simplify(ego)
+ego <- simplify(ego)
+
+top10_gene <- rate_tbl %>% slice_head(prop = 0.1) %>% pull(gene_id)
+bottom10_gene <- rate_tbl %>% slice_tail(prop = 0.1) %>% pull(gene_id) 
+
+ego2 <- enrichGO(gene         = top10_gene,
+                 OrgDb         = org.Hs.eg.db,
+                 keyType       = 'ENSEMBL',
+                 ont           = "BP",
+                 pAdjustMethod = "BH",
+                 pvalueCutoff  = 0.01,
+                 qvalueCutoff  = 0.05)
+
+# KEGG Gene Set Enrichment Analysis
+id_map <- bitr(names(geneList), fromType = "ENSEMBL", toType = "ENTREZID", OrgDb = org.Hs.eg.db)
+id_map <- id_map[!duplicated(id_map$ENSEMBL), ]
+
+rate_tbl <- rate_tbl %>%
+  dplyr::left_join(id_map, by = c("gene_id" = "ENSEMBL"))
+
+geneList2 <- rate_tbl$alpha
+names(geneList2) <- rate_tbl$ENTREZID
+
+kk <- gseKEGG(geneList     = geneList2,
+               organism     = 'hsa',
+               minGSSize    = 100,
+               pvalueCutoff = 0.5,
+               verbose      = FALSE,
+               scoreType = "pos")
 
 #### Visualization of Functional Enrichment Result ####
 # Bar Plot
-library(enrichplot)
 
 
+# GSEA
 dotplot(ego, showCategory=30) + ggtitle("dotplot for GSEA")
-plot_grid(p1, p2, ncol=2)
+dotplot(kk, showCategory=30) + ggtitle("dotplot for GSEA")
+# 
+dotplot(ego2, showCategory=30) + ggtitle("dotplot for GSEA")
+
 
 # Gene-Concept Network
 ## convert gene ID to Symbol
-edox <- setReadable(edo, 'org.Hs.eg.db', 'ENTREZID')
+edox <- setReadable(ego, 'org.Hs.eg.db', 'ENSEMBL')
+
 p1 <- cnetplot(edox, foldChange=geneList)
 ## categorySize can be scaled by 'pvalue' or 'geneNum'
 p2 <- cnetplot(edox, categorySize="pvalue", foldChange=geneList)
 p3 <- cnetplot(edox, foldChange=geneList, circular = TRUE, colorEdge = TRUE)
 cowplot::plot_grid(p1, p2, p3, ncol=3, labels=LETTERS[1:3], rel_widths=c(.8, .8, 1.2))
-
-p1 <- cnetplot(edox, node_label="category")
-p2 <- cnetplot(edox, node_label="gene")
-p3 <- cnetplot(edox, node_label="all")
-p4 <- cnetplot(edox, node_label="none")
-cowplot::plot_grid(p1, p2, p3, p4, ncol=2, labels=LETTERS[1:4])
 
 # Heatmap-like functional classification
 p1 <- heatplot(edox)
@@ -76,78 +112,54 @@ p2 <- heatplot(edox, foldChange=geneList)
 cowplot::plot_grid(p1, p2, ncol=1, labels=LETTERS[1:2])
 
 # Enrichment Map
-edo <- pairwise_termsim(edo)
-p1 <- emapplot(edo)
-p2 <- emapplot(edo, pie_scale=1.5)
-p3 <- emapplot(edo,layout="kk")
-p4 <- emapplot(edo, pie_scale=1.5,layout="kk")
-cowplot::plot_grid(p1, p2, p3, p4, ncol=2, labels=LETTERS[1:4])
-
-library(clusterProfiler)
-data(gcSample)
-xx <- compareCluster(gcSample, fun="enrichKEGG",
-                     organism="hsa", pvalueCutoff=0.05)
-xx <- pairwise_termsim(xx)
-p1 <- emapplot(xx)
-p2 <- emapplot(xx,legend_n=2)
-p3 <- emapplot(xx,pie="count")
-p4 <- emapplot(xx,pie="count", pie_scale=1.5, layout="kk")
+ego <- pairwise_termsim(ego)
+p1 <- emapplot(ego)
+p2 <- emapplot(ego, pie_scale=1.5)
+p3 <- emapplot(ego,layout="kk")
+p4 <- emapplot(ego, pie_scale=1.5,layout="kk")
 cowplot::plot_grid(p1, p2, p3, p4, ncol=2, labels=LETTERS[1:4])
 
 # UpSet Plot
-upsetplot(edo)
-
-upsetplot(kk2)
+upsetplot(ego)
 
 # ridgeline plot for expression distribution of GSEA result
-ridgeplot(edo2)
+ridgeplot(ego)
 
 # running score and preranked list of GSEA result
-p1 <- gseaplot(edo2, geneSetID = 1, by = "runningScore", title = edo2$Description[1])
-p2 <- gseaplot(edo2, geneSetID = 1, by = "preranked", title = edo2$Description[1])
-p3 <- gseaplot(edo2, geneSetID = 1, title = edo2$Description[1])
+p1 <- gseaplot(ego, geneSetID = 1, by = "runningScore", title = ego$Description[1])
+p2 <- gseaplot(ego, geneSetID = 1, by = "preranked", title = ego$Description[1])
+p3 <- gseaplot(ego, geneSetID = 1, title = ego$Description[1])
 cowplot::plot_grid(p1, p2, p3, ncol=1, labels=LETTERS[1:3])
 
-gseaplot2(edo2, geneSetID = 1, title = edo2$Description[1])
-gseaplot2(edo2, geneSetID = 1:3)
+gseaplot2(ego, geneSetID = 1, title = ego$Description[1])
+gseaplot2(ego, geneSetID = 1:3)
 
-gseaplot2(edo2, geneSetID = 1:3, pvalue_table = TRUE,
+gseaplot2(ego, geneSetID = 1:3, pvalue_table = TRUE,
           color = c("#E495A5", "#86B875", "#7DB0DD"), ES_geom = "dot")
 
-p1 <- gseaplot2(edo2, geneSetID = 1:3, subplots = 1)
-p2 <- gseaplot2(edo2, geneSetID = 1:3, subplots = 1:2)
+p1 <- gseaplot2(ego, geneSetID = 1:3, subplots = 1)
+p2 <- gseaplot2(ego, geneSetID = 1:3, subplots = 1:2)
 cowplot::plot_grid(p1, p2, ncol=1, labels=LETTERS[1:2])
 
-gsearank(edo2, 1, title = edo2[1, "Description"])
+gsearank(ego, 1, title = ego[1, "Description"])
 
 library(ggplot2)
 library(cowplot)
 
 pp <- lapply(1:3, function(i) {
-  anno <- edo2[i, c("NES", "pvalue", "p.adjust")]
+  anno <- ego[i, c("NES", "pvalue", "p.adjust")]
   lab <- paste0(names(anno), "=",  round(anno, 3), collapse="\n")
   
-  gsearank(edo2, i, edo2[i, 2]) + xlab(NULL) +ylab(NULL) +
-    annotate("text", 0, edo2[i, "enrichmentScore"] * .9, label = lab, hjust=0, vjust=0)
+  gsearank(ego, i, ego[i, 2]) + xlab(NULL) +ylab(NULL) +
+    annotate("text", 0, ego[i, "enrichmentScore"] * .9, label = lab, hjust=0, vjust=0)
 })
 plot_grid(plotlist=pp, ncol=1)
 
 # pubmed trend of enriched terms
-terms <- edo$Description[1:3]
+terms <- ego$Description[1:3]
 p <- pmcplot(terms, 2010:2017)
 p2 <- pmcplot(terms, 2010:2017, proportion=FALSE)
 plot_grid(p, p2, ncol=2)
 
 # goplot
-goplot(ego)
-
-# browseKEGG
-browseKEGG(kk, 'hsa04110')
-
-# pathview from pathview package
-library("pathview")
-# https://bioconductor.org/packages/release/bioc/html/pathview.html
-hsa04110 <- pathview(gene.data  = geneList,
-                     pathway.id = "hsa04110",
-                     species    = "hsa",
-                     limit      = list(gene=max(abs(geneList)), cpd=1))
+# goplot(ego)
